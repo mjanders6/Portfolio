ba5be23cf7732241b75bfe8d48cf8b8c0ff0eda2
Mike <mjanders6@gmail.com>
2019-07-28 14:19:55 -0700

    update to jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
 
-function App() {
+import React from 'react';
+import { Jumbotron, Container, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+
+const Home = (props) => {
   return (
-    <div className="App">
-      <h1>ALOHA WORLD!</h1>
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-2">Aloha!</h1>
+          <h1 className="display-5">And welcome to my landing page.</h1>
+          <p className="lead">I am in the process of building out the website. In the meantime, check me out on Linkedin and Github</p>
+          <hr className="my-2" />
+          <p><a href='https://www.linkedin.com/in/mjanders6'>Linkedin</a> | <a href='https://github.com/mjanders6'>Github</a></p>
+        </Container>
+      </Jumbotron>
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
